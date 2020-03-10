215a0ca1269307218070034d5c7da707f1399421
rfreitas <rfreitas@squaretrade.com>
2020-03-10 16:10:40 -0300

    Added Lambda handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
+extern crate log;
+
 mod lambda_api;
 
-fn main() {
-    println!("Hello, world!");
+use lambda_api::*;
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use serde_json;
+use simple_logger;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(log::Level::Info)?;
+    lambda!(api_gateway_handler);
+    Ok(())
+}
+
+pub fn api_gateway_handler(request: ApiRequest, _c: Context) -> Result<ApiResponse, HandlerError> {
+    let body: RequestBody = serde_json::from_str(&request.body).unwrap_or(RequestBody {
+        ..Default::default()
+    });
+
+    log::info!("{}", &body.name);
+
+    let response = ApiResponse {
+        status_code: 200,
+        body: format!("Hello, {} from {}!", body.name, body.city),
+        ..Default::default()
+    };
+    
+    Ok(response)
 }
